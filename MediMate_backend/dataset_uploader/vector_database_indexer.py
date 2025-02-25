66ba5745a9be4fe1c1220575f99c8dd964b164c7
Pietro <c1022206@newcastle.ac.uk>
2025-02-25 15:48:23 +0000

    create two vector database retirival queries, for specific ID extranction and relevance retrival

--- MediMate_backend/dataset_uploader/vector_database_indexer.py
+++ MediMate_backend/dataset_uploader/vector_database_indexer.py
@@ -34,7 +34,7 @@ def index_allergy_data(df):
     df, allergy_embeddings = generate_embeddings(df, ['DESCRIPTION', 'TYPE', 'CATEGORY', 'REACTION1', 'DESCRIPTION1', 'REACTION2', 'DESCRIPTION2'])
     for i, row in df.iterrows():
         allergy_collection.add(
-            ids=[str(row['PATIENT'])],
+            ids=[str(i)],
             embeddings=[allergy_embeddings[i].tolist()],
             documents=[row['index_text']],
             metadatas=[row.to_dict()]
@@ -44,7 +44,7 @@ def index_conditions_data(df):
     df, conditions_embeddings = generate_embeddings(df, ['DESCRIPTION'])
     for i, row in df.iterrows():
         condition_collection.add(
-            ids=[str(row['PATIENT'])],
+            ids=[str(i)],
             embeddings=[conditions_embeddings[i].tolist()],
             documents=[row['index_text']],
             metadatas=[row.to_dict()]
@@ -54,7 +54,7 @@ def index_encounters_data(df):
     df, encounters_embeddings = generate_embeddings(df, ['ENCOUNTERCLASS', 'DESCRIPTION', 'REASONDESCRIPTION'])
     for i, row in df.iterrows():
         encounter_collection.add(
-            ids=[str(row['PATIENT'])],
+            ids=[str(i)],
             embeddings=[encounters_embeddings[i].tolist()],
             documents=[row['index_text']],
             metadatas=[row.to_dict()]
@@ -64,11 +64,31 @@ def index_medications_data(df):
     df, medications_embeddings = generate_embeddings(df, ['DESCRIPTION', 'REASONDESCRIPTION'])
     for i, row in df.iterrows():
         medication_collection.add(
-            ids=[str(row['PATIENT'])],
+            ids=[str(i)],
             embeddings=[medications_embeddings[i].tolist()],
             documents=[row['index_text']],
             metadatas=[row.to_dict()]
         )
+
+def rag_entry_point(patient_id, query_text):
+    allergy_collection = chroma_client.get_collection(name='allergy_collection')
+    condition_collection = chroma_client.get_collection(name='condition_collections')
+    encounter_collection = chroma_client.get_collection(name='encounter_collection')
+    medication_collection = chroma_client.get_collection(name='medication_collection')
+    
+def patient_record_collector(patient_id, collection_obj):
+     results = collection_obj.get(where={"PATIENT": patient_id})
+     return results
+
+def similar_record_context_search(query_text, collection_obj, top_k=2):
+    query_embedding = model.encode([query_text]).tolist()
+    results = collection_obj.query(
+        query_embeddings = query_embedding,
+        n_results = top_k,
+        include = ["documents", "metadatas", "distances"]
+    )
+    return results
+    
 
 chroma_client = chromadb.PersistentClient(path='./chroma_db')
 allergy_collection = chroma_client.get_or_create_collection(name='allergy_collection')
@@ -76,4 +96,5 @@ condition_collection = chroma_client.get_or_create_collection(name='condition_co
 encounter_collection = chroma_client.get_or_create_collection(name='encounter_collection')
 medication_collection = chroma_client.get_or_create_collection(name='medication_collection')
 model = SentenceTransformer('all-MiniLM-L6-v2')
-print(allergy_collection.count(), condition_collection.count(), encounter_collection.count(), medication_collection.count())
+
+print(patient_record_collector('4569671e-ed39-055f-8e78-422b96c9896b', allergy_collection))
