4909849647d50c7cd7a7a4d3426b3afd00ce40e3
Pietro <c1022206@newcastle.ac.uk>
2025-03-17 11:06:15 +0000

    wrangling code for drug drug interaction dataset, preparing it google collab finetuning.

--- MediMate_frontend/src/components/HeroBanner.vue
+++ MediMate_frontend/src/components/HeroBanner.vue
@@ -3,13 +3,13 @@
         <div class="hero-banner__content">
           <div class="hero-banner__heading">
             <h2 class="heading heading__h1" v-if="!userStore.user.isAuthenticated">Welcome to <span style="color: rgba(0, 123, 255, 0.8); font-weight: 600">MediMate</span></h2>
-            <h2 class="heading heading__h1" v-if="userStore.user.isAuthenticated">Welcome {{userStore.user.name}},</h2>
-            <p class="heading heading__h3">AI-powered Medical Agent</p>
+            <h2 class="heading heading__h1" v-if="userStore.user.isAuthenticated">Welcome <span style="color: rgba(0, 123, 255, 0.8); font-weight: 600">{{userStore.user.name}},</span></h2>
+            <p class="heading heading__h3">Get Started with MediMate</p>
           </div>
           <div class="hero-banner__subheading">
-            <p class="heading heading__h4" v-if="!userStore.user.isAuthenticated">EHR Summarisation, Prescription Clash Detection <br> and Diagnosis Validation
+            <p class="heading heading__h4" v-if="!userStore.user.isAuthenticated">Agent for EHR Summarisation, Prescription Clash Detection <br> and Diagnosis Validation
             </p>
-            <p class="heading heading__p" v-if="userStore.user.isAuthenticated">Enter your doctors office to get started with MediMate</p>
+            <p class="heading heading__p" v-if="userStore.user.isAuthenticated">Enter your doctor's office</p>
             <button class="button button--primary" v-if="!userStore.user.isAuthenticated" @click="redirect('/doctors-office')">Get Started</button>
             <button class="button button--primary" v-if="userStore.user.isAuthenticated" @click="redirect('/doctors-office')">Doctors Office</button>
           </div>
